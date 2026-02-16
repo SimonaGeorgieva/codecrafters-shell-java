f64c5cb2b7073be829e21e21fb7463744e21edb2
Simona Georgieva <simona.georgieva096@gmail.com>
2026-02-16 16:53:38 +0200

    Implement exit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Main {
+
+    public static final String EXIT = "exit";
+
     static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String userMessage;
@@ -8,6 +11,10 @@ public class Main {
         while (true) {
             System.out.print("$ ");
             userMessage = scanner.nextLine();
+
+            if (userMessage != null && EXIT.equals(userMessage.trim())) {
+                break;
+            }
             System.out.printf("%s: command not found%n", userMessage);
         }
     }
