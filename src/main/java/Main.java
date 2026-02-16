011fb1db255f251f88337485201c278ee052c692
Simona Georgieva <simona.georgieva096@gmail.com>
2026-02-16 16:26:38 +0200

    Handle invalid commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    static void main(String[] args) {
         System.out.print("$ ");
+
+        Scanner scanner = new Scanner(System.in);
+        String userMessage = scanner.nextLine();
+
+        System.out.printf("%s: command not found%n", userMessage);
     }
 }
