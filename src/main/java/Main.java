9831d01fb3406d71a4819c0638a86b900a77c17c
Simona Georgieva <simona.georgieva096@gmail.com>
2026-02-17 18:38:12 +0200

    Implement type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,22 @@
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 
     public static final String DEFAULT_LINE = "$ ";
     public static final String EXIT = "exit";
     public static final String ECHO = "echo";
+    public static final String TYPE = "type";
+    public static final Set<String> builtins = new HashSet<>(List.of(EXIT, ECHO, TYPE));
     public static final String SEPARATOR = " ";
 
     static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String commandLine;
         String command;
+        String operand;
 
         while (true) {
             System.out.print(DEFAULT_LINE);
@@ -21,9 +27,18 @@ public class Main {
             }
             commandLine = commandLine.trim();
             command = commandLine.split(SEPARATOR)[0];
+            operand = commandLine.substring(command.length()).trim();
 
             if (ECHO.equals(command)) {
-                System.out.println(commandLine.substring(command.length()).trim());
+                System.out.println(operand);
+                continue;
+            }
+
+            if (TYPE.equals(command)) {
+                if (operand.isBlank()) {
+                    continue;
+                }
+                System.out.printf(builtins.contains(operand) ? "%s is a shell builtin%n" : "%s: not found%n", operand);
                 continue;
             }
 
