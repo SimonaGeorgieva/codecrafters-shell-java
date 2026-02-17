424432a77dc046577fb4a9d5c84027da67121e37
Simona Georgieva <simona.georgieva096@gmail.com>
2026-02-17 18:08:23 +0200

    Implement echo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,20 +2,35 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static final String DEFAULT_LINE = "$ ";
     public static final String EXIT = "exit";
+    public static final String ECHO = "echo";
+    public static final String SEPARATOR = " ";
 
     static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String userMessage;
+        String commandLine;
+        String command;
 
         while (true) {
-            System.out.print("$ ");
-            userMessage = scanner.nextLine();
+            System.out.print(DEFAULT_LINE);
+            commandLine = scanner.nextLine();
 
-            if (userMessage != null && EXIT.equals(userMessage.trim())) {
+            if (commandLine == null || commandLine.isBlank()) {
+                continue;
+            }
+            commandLine = commandLine.trim();
+            command = commandLine.split(SEPARATOR)[0];
+
+            if (ECHO.equals(command)) {
+                System.out.println(commandLine.substring(command.length()).trim());
+                continue;
+            }
+
+            if (EXIT.equals(command)) {
                 break;
             }
-            System.out.printf("%s: command not found%n", userMessage);
+            System.out.printf("%s: command not found%n", commandLine);
         }
     }
 }
